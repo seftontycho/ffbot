b6b66d0691af3909f47cdcc0c03c27cb9b7ff152
Sefton de Pledge <seftontycho@hotmail.co.uk>
2025-01-01 01:45:24 +0000

    batman

---
+++ scrape.py
@@ -0,0 +1,20 @@
+from seleniumbase import SB
+import time
+
+with SB(uc=True, test=True, locale_code="fr", headed=True, incognito=True) as sb:
+    url = "https://gitlab.com/users/sign_in"
+    sb.activate_cdp_mode(url)
+    sb.sleep(1)
+    sb.uc_gui_click_captcha()
+    sb.sleep(2)
+    sb.save_screenshot_to_logs()
+
+time.sleep(3)
+
+with SB(uc=True, test=True, locale_code="fr", headed=True) as sb:
+    url = "https://gitlab.com/users/sign_in"
+    sb.activate_cdp_mode(url)
+    sb.sleep(1)
+    sb.cdp.gui_click_element("#HJup0 > div")
+    sb.sleep(2)
+    sb.save_screenshot_to_logs()
