ad248078c6594d9e85a7ec9e366200f1aef54253
Sefton de Pledge <seftontycho@hotmail.co.uk>
2025-01-01 02:05:56 +0000

    update

--- scrape.py
+++ scrape.py
@@ -1,6 +1,8 @@
 from seleniumbase import SB
 import time
 
+SELECTOR = "body > div.gl-h-full.gl-flex.gl-flex-wrap > div.container.gl-self-center > div > div.gl-my-5 > div.col-sm-12.gl-text-center > h1"
+
 with SB(uc=True, test=True, locale_code="fr", headed=False, incognito=True) as sb:
     url = "https://gitlab.com/users/sign_in"
     sb.activate_cdp_mode(url)
@@ -8,6 +10,8 @@ with SB(uc=True, test=True, locale_code="fr", headed=False, incognito=True) as s
     sb.uc_gui_click_captcha()
     sb.sleep(2)
     sb.save_screenshot_to_logs()
+    if sb.is_element_present():
+        print("It worked! Found the element using v1!")
 
 time.sleep(3)
 
@@ -18,3 +22,5 @@ with SB(uc=True, test=True, locale_code="fr", headed=False) as sb:
     sb.cdp.gui_click_element("#HJup0 > div")
     sb.sleep(2)
     sb.save_screenshot_to_logs()
+    if sb.is_element_present():
+        print("It worked! Found the element using v2!")
